5335c88812ece80e832e88e2f91cf8b6a3405eb7
IT24102690 <IT24102690@my.sliit.lk>
2025-09-04 01:12:09 +0530

    lab 6

---
+++ Lab Sheet 06/IT24102690.R
@@ -0,0 +1,16 @@
+setwd("C:\\Users\\User\\Desktop\\LAB 6")
+getwd()
+
+#Question 1
+#i. The distribution of X is a binomial distribution
+
+#ii.
+1-pbinom(46,50,0.85, lower.tail = TRUE)
+
+#Question 2
+#i. Number of calls recieved by the call center per hour
+
+#ii. The distribution is poisson
+
+#iii. P(X=15)
+dpois(15,lambda=12)
